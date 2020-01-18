1b12df00621dbf285604a56f2e440cb8f4bb4160
birukSebsibe <bksebsibe55@gmail.com>
2020-01-18 15:47:00 +0300

    home commit

---
+++ home.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+	<link rel="stylesheet" type="text/css" href="home.css ">
+</head>
+<body  >
+					<img src="img/photo.jpg"width="1500" height="150">
+		<div class="menu-bar">
+		<ul>
+		<li class="active"><a href="home.html"><strong>HOME</strong></a></li>
+		<li><a href="#"><strong>LOGIN</strong></a>
+         <div class="sub-menu-1">
+         	<ul>
+         		<li><a href="index.php">FAMILY</a></li>
+         		<li><a href="index2.php">STUDENT</a></li>
+            <li><a href="index2.php">TEACHER</a></li>
+         	</ul>
+
+         </div>
+
+		</li>
+	    <li><a href="contact.php"><strong>CONTACT</strong></a></li>
+	    <li><a href="#"><strong>ABOUT</strong></a>
+	    <div class="sub-menu-2">
+         	<ul>
+         		<li><a href="aschool.php">SCHOOL</a></li>
+         		<li><a href="adevelop.php">DEVELOPERS</a></li>
+         	</ul>
+
+         </div>
+         </li> 
+        <div class="news">
+         <p><strong><h1>NEWS FEED</h1></strong></p>
+</div>
+
+<div class="news-2">
+         <p><strong><h1>UPCOMING EVENTS</h1></strong></p>
+</div>
+        <div class="carde-6">
+          <span></span><span></span><span></span><span></span>
+          <div class="content-6"> 
+           <h2>designed Bkstudio</h2>
+           <p>it is a software campany that is used to develop a computer system</p>
+           <a href="#">Read more</a>
+          </div>
+        </div>
+
+
+
+          <div class="carde-5">
+          <span></span><span></span><span></span><span></span>
+          <div class="content-5"> 
+           <h2>designed Bkstudio</h2>
+           <p>it is a software campany thaa computer system</p>
+           <a href="read-2.html">Read more</a>
+          </div>
+        </div>
+
+          <div class="carde">
+          <span></span><span></span><span></span><span></span>
+          <div class="content-4"> 
+           <h2>designed Bkstudio</h2>
+           <p>it is a software campany that is used to develop a computer system</p>
+           <a href="#">Read more</a>
+          </div>
+        </div>
+
+        <div class="card">
+        	<span></span><span></span><span></span><span></span>
+        	<div class="content">
+        	<h2>girls basket ball team</h2>
+        	<p>saint josph girls basket ball will paricipate for .....</p>
+        	<a href="read-1.html">Read more</a>
+        </div>
+        </div>
+        <div class="card-2" >
+        	<span></span><span></span><span></span><span></span>
+        	<div class="content-2"> 
+           <h2>designed Bkstudio</h2>
+           <p>it is a software campany that is used to develop a computer system</p>
+           <a href="read-1.html">Read more</a>
+        	</div>
+        </div>
+        <div class="card-3">
+        	<span></span><span></span><span></span><span></span>
+        	<div class="content-3"> 
+           <h2>designed Bkstudio</h2>
+           <p>it is a software campany that is used to develop a computer system</p>
+           <a href="read-1.html">Read more</a>
+        </div>
+       
+<div class="contact">
+	<h4>contact us </h4>
+	<p>  call   email    facebook </p>
+</div>
+
+</div>
+<div class="footer">
+	<div class="footer-section">
+	<div class="footer-content">
+		<div class="footer-section-about">
+			<h3>about this website</h3>
+			<h4><p>this website is design to help families and students
+       <br>to help both families and students, families can 
+       <br>access there childers acadamic result and make payment
+       <br>online student can make registeration and access thier
+       <br>assessment online.
+       <br>
+       <br>this website  id designed for educational service the owner
+       <br>of the sit is saint joseph school adama branch
+       <br>the development of the site is done by Smart technologies
+       <br>
+       <br> 
+       <br>SAINT JOSPHE SCHOOL ADAMA
+         </p></h4>
+
+      </p>
+		</div>
+		<div class="footer-section-link">
+			<h3>links to our school</h3>
+     <li> <i class="fab fa-facebook" aria-hidden="true">face book</i></li>
+     <i class="fab fa-facebook"></i>
+			
+			<a href="facebook.com">tweeter </a>
+		</div>
+		<div class="footer-section-contact">
+			<h3>you can contact us </h3>
+			<p>phone  0978654356</p>
+			<p>email  bksebsibe55@gmail.com</p>
+		</div>
+	<div class="footer-bottem">
+		&copy; smartthcnologies.com| designed by SMART THECNOLOGIES
+	</div>
+</div>
+</div>
+</div>
+</ul>
+</div>
+
+</body>
+</html>
